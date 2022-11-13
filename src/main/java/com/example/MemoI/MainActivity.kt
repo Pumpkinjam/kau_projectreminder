dc4de5aaac99e3207821e6947550b5f773527841
Pumpkinjam <rnjsl1212@naver.com>
2022-11-13 16:16:10 +0900

    file save/load almost done.

--- src/main/java/com/example/MemoI/MainActivity.kt
+++ src/main/java/com/example/MemoI/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.MemoI
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.example.MemoI.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import java.io.*
 import java.nio.Buffer
 import java.nio.file.Files
@@ -13,20 +18,18 @@ import java.nio.file.Paths
 import java.util.*
 
 // TODO: add button clickListners
+// TODO: don't let this activity create/destroy while using app
 class MainActivity : AppCompatActivity() {
 
-    // TODO: todoList logic, and save the information to file(csv format)
+    // DO-ing: todoList logic, and save the information to file(csv format)
     // TODO+: encryption?
     lateinit var todoList: Vector<Todo>
+    lateinit var activityStack: Stack<ActivityCompat>
     lateinit var binding: ActivityMainBinding
-    lateinit var savefile: File
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        todoList = Vector<Todo>()
-
-        var dirpath = Environment.getExternalStorageDirectory().getAbsolutePath()
+    //lateinit var savefile: File
+/*
+    fun loadFile() {
+        var dirpath = Environment.getExternalStorageDirectory().absolutePath
 
         println("Checking file directories...")
         for (dir in Todo.fileDirArray) {
@@ -74,10 +77,71 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
 
+    }
+    */
+
+    private fun loadFile() {
+
+        var files: Array<String> = this.fileList()
+        if (Todo.filename in files) println("${Todo.filename} found!")
+        else this.openFileOutput(Todo.filename, Context.MODE_PRIVATE).use {
+            println("Creating new file...")
+            it.write(" ".toByteArray())
+        }
+
+        val lines = this.openFileInput(Todo.filename).bufferedReader().readLines()
+        for (line in lines) {
+            if (line == " ") break
+            todoList.add(Todo.of(line))
+        }
+        println("load complete.")
+    }
+    private fun reqPermission(): Boolean {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_GRANTED)
+        {
+            println("Permission already granted.")
+            return true
+        }
+        else {
+            println("requesting permission")
+            ActivityCompat.requestPermissions(this,
+                arrayOf<String>(
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.MANAGE_EXTERNAL_STORAGE
+                ), 1);
+        }
+
+        return (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_GRANTED)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        todoList = Vector<Todo>()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (reqPermission()) loadFile()
+
+/*
+    This code not works...
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()
+            ) {
+                isGranted: Boolean ->
+                if (isGranted) {
+                    println("===============wow")
+                    loadFile()
+                } else {
+                   // Snackbar.make(binding.root, "sure?", Snackbar.LENGTH_SHORT).show()
+                    println("=================nope")
+                }
+            }
+*/
         binding.addMore.setOnClickListener {
             todoList.add(Todo("title!", "Des!", ))
             System.out.println(todoList.get(0))
@@ -91,14 +155,12 @@ class MainActivity : AppCompatActivity() {
         // TODO: imagine that what happens if the saving progress fails...
         // TODO: we need something more safe way
         try {
-            val bw = BufferedWriter(FileWriter(savefile))
-
             // must save " " at the end of the file!!
             var str = ""
             for (td in todoList) str += td.toCsvFormat()
             str += " \n"
 
-            bw.write(str)
+            this.openFileOutput(Todo.filename, Context.MODE_PRIVATE).write(str.toByteArray())
 
             System.out.println("Todo List save completed.")
         }
